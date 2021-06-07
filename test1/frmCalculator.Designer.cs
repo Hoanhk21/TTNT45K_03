5a1f4cceb53cb5ea8e98cb682a73ddc881a4468d
Hoanhk21 <hoanhtnq45k21.1@gmail.com>
2021-06-07 20:44:20 +0700

    Hoanhtnq_Calculator

---
+++ test1/frmCalculator.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace test1
+{
+    partial class frmCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btncong = new System.Windows.Forms.Button();
+            this.btntru = new System.Windows.Forms.Button();
+            this.btnnhan = new System.Windows.Forms.Button();
+            this.btnchia = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txtKQ = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btncong
+            // 
+            this.btncong.Location = new System.Drawing.Point(31, 308);
+            this.btncong.Name = "btncong";
+            this.btncong.Size = new System.Drawing.Size(75, 23);
+            this.btncong.TabIndex = 0;
+            this.btncong.Text = "Cộng";
+            this.btncong.UseVisualStyleBackColor = true;
+            this.btncong.Click += new System.EventHandler(this.btncong_Click);
+            // 
+            // btntru
+            // 
+            this.btntru.Location = new System.Drawing.Point(228, 308);
+            this.btntru.Name = "btntru";
+            this.btntru.Size = new System.Drawing.Size(75, 23);
+            this.btntru.TabIndex = 1;
+            this.btntru.Text = "Trừ";
+            this.btntru.UseVisualStyleBackColor = true;
+            this.btntru.Click += new System.EventHandler(this.btntru_Click);
+            // 
+            // btnnhan
+            // 
+            this.btnnhan.Location = new System.Drawing.Point(402, 308);
+            this.btnnhan.Name = "btnnhan";
+            this.btnnhan.Size = new System.Drawing.Size(75, 23);
+            this.btnnhan.TabIndex = 2;
+            this.btnnhan.Text = "Nhân";
+            this.btnnhan.UseVisualStyleBackColor = true;
+            this.btnnhan.Click += new System.EventHandler(this.btnnhan_Click);
+            // 
+            // btnchia
+            // 
+            this.btnchia.Location = new System.Drawing.Point(595, 308);
+            this.btnchia.Name = "btnchia";
+            this.btnchia.Size = new System.Drawing.Size(75, 23);
+            this.btnchia.TabIndex = 3;
+            this.btnchia.Text = "Chia";
+            this.btnchia.UseVisualStyleBackColor = true;
+            this.btnchia.Click += new System.EventHandler(this.btnchia_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(2)))));
+            this.txt1.Location = new System.Drawing.Point(114, 92);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 20);
+            this.txt1.TabIndex = 4;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(413, 92);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 20);
+            this.txt2.TabIndex = 5;
+            // 
+            // txtKQ
+            // 
+            this.txtKQ.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKQ.ForeColor = System.Drawing.Color.Red;
+            this.txtKQ.Location = new System.Drawing.Point(277, 197);
+            this.txtKQ.Name = "txtKQ";
+            this.txtKQ.Size = new System.Drawing.Size(100, 29);
+            this.txtKQ.TabIndex = 6;
+            // 
+            // frmCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtKQ);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.btnchia);
+            this.Controls.Add(this.btnnhan);
+            this.Controls.Add(this.btntru);
+            this.Controls.Add(this.btncong);
+            this.Name = "frmCalculator";
+            this.Text = "frmCalculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btncong;
+        private System.Windows.Forms.Button btntru;
+        private System.Windows.Forms.Button btnnhan;
+        private System.Windows.Forms.Button btnchia;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txtKQ;
+    }
+}
+
---
+++ test1/frmCalculator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test1
+{
+    public partial class frmCalculator : Form
+    {
+        public frmCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void btncong_Click(object sender, EventArgs e)
+        {
+            string SSO1 = txt1.Text;
+            decimal dSO1 = Convert.ToDecimal(SSO1);
+            string SSO2 = txt2.Text;
+            decimal dSO2 = Convert.ToDecimal(SSO2);
+            decimal dKQ = dSO1 + dSO2;
+            txtKQ.Text = dKQ.ToString();
+        }
+
+        private void btntru_Click(object sender, EventArgs e)
+        {
+            string SSO1 = txt1.Text;
+            decimal dSO1 = Convert.ToDecimal(SSO1);
+            string SSO2 = txt2.Text;
+            decimal dSO2 = Convert.ToDecimal(SSO2);
+            decimal dKQ = dSO1 - dSO2;
+            txtKQ.Text = dKQ.ToString();
+        }
+
+        private void btnnhan_Click(object sender, EventArgs e)
+        {
+            string SSO1 = txt1.Text;
+            decimal dSO1 = Convert.ToDecimal(SSO1);
+            string SSO2 = txt2.Text;
+            decimal dSO2 = Convert.ToDecimal(SSO2);
+            decimal dKQ = dSO1 * dSO2;
+            txtKQ.Text = dKQ.ToString();
+        }
+
+        private void btnchia_Click(object sender, EventArgs e)
+        {
+            string SSO1 = txt1.Text;
+            decimal dSO1 = Convert.ToDecimal(SSO1);
+            string SSO2 = txt2.Text;
+            decimal dSO2 = Convert.ToDecimal(SSO2);
+            decimal dKQ = dSO1 / dSO2;
+            txtKQ.Text = dKQ.ToString();
+        }
+    }
+}
