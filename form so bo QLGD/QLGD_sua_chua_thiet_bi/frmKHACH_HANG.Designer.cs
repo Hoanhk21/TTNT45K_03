bc1991c86ec2df47918faabdc3989c1593a5803f
trucly45k21-1 <lytudiepthao2117@gmail.com>
2021-06-14 17:00:47 +0700

    form so bo QLGD_nhom 3

---
+++ form so bo QLGD/QLGD_sua_chua_thiet_bi/frmKHACH_HANG.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace QLGD_sua_chua_thiet_bi
+{
+    partial class frmKHACH_HANG
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmKHACH_HANG
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "frmKHACH_HANG";
+            this.Text = "frmKHACH_HANG";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ form so bo QLGD/QLGD_sua_chua_thiet_bi/frmKHACH_HANG.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLGD_sua_chua_thiet_bi
+{
+    public partial class frmKHACH_HANG : Form
+    {
+        public frmKHACH_HANG()
+        {
+            InitializeComponent();
+        }
+    }
+}
